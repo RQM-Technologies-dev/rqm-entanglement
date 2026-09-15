6faf16760a894ec33b29d496268f4eca12607dc1
RQM Technologies <JVG@RQMtechnologies.com>
2026-09-15 10:08:02 -0400

    feat: export adaptive relational IR

--- src/rqm_entanglement/__init__.py
+++ src/rqm_entanglement/__init__.py
@@ -1,33 +1,4 @@
-"""rqm_entanglement – two-qubit / nonlocal layer of the RQM stack.
-
-Public API
-----------
-Constants & tolerances:
-    ATOL, RTOL, I2, I4, X, Y, Z, XX, YY, ZZ
-    CNOT, CZ, SWAP, ISWAP
-
-Basis states:
-    ket00, ket01, ket10, ket11, basis_state, computational_basis
-
-Tensor helpers:
-    kron, local_unitary, apply_unitary
-
-State helpers:
-    normalize_state, state_from_amplitudes, density_matrix, reduced_density_matrix
-
-Canonical entanglers:
-    xx_rotation, yy_rotation, zz_rotation, canonical_entangler
-
-Measures:
-    concurrence_pure, is_separable_pure, schmidt_values_pure
-    von_neumann_entropy, entanglement_entropy_pure
-
-Classification:
-    operator_schmidt_rank, is_local_product_operator, local_product_factors
-
-Adapter:
-    rqm_core_available, local_from_quaternions, su2_from_quaternion_components
-"""
+"""rqm_entanglement – two-qubit / nonlocal layer of the RQM stack."""
 
 from rqm_entanglement.adapters.rqm_core_adapter import (
     canonicalize_quaternion_sign_with_phase,
@@ -94,6 +65,18 @@ from rqm_entanglement.measures import (
     schmidt_values_pure,
     von_neumann_entropy,
 )
+from rqm_entanglement.relational import (
+    IDENTITY_QUATERNION,
+    AxisHinge,
+    BellHinge,
+    CartanRelation,
+    RelationalLevel,
+    RelationalOperator,
+    compose_relations,
+    compress_unitary,
+    promote_with_local_frames,
+    relational_level,
+)
 from rqm_entanglement.states import (
     density_matrix,
     normalize_state,
@@ -128,93 +111,30 @@ from rqm_entanglement.validation import (
 )
 
 __all__ = [
-    # tolerances
-    "ATOL",
-    "RTOL",
-    # constants
-    "I2",
-    "I4",
-    "X",
-    "Y",
-    "Z",
-    "XX",
-    "YY",
-    "ZZ",
-    "CNOT",
-    "CZ",
-    "SWAP",
-    "ISWAP",
-    # basis
-    "ket00",
-    "ket01",
-    "ket10",
-    "ket11",
-    "basis_state",
-    "computational_basis",
-    # tensor
-    "kron",
-    "local_unitary",
-    "apply_unitary",
-    # states
-    "normalize_state",
-    "state_from_amplitudes",
-    "density_matrix",
-    "reduced_density_matrix",
-    # canonical
-    "xx_rotation",
-    "yy_rotation",
-    "zz_rotation",
-    "canonical_entangler",
-    "cartan_core_from_weyl",
-    "weyl_to_rotation_coordinates",
-    "rotation_to_weyl_coordinates",
-    # arbitrary SU(4)
-    "CONVENTION_VERSION",
-    "QuaternionCartanBlock",
-    "SU4Classification",
-    "VerifiedSU4Decomposition",
-    "decompose_su4",
-    "decompose_su4_verified",
-    "reconstruct_su4",
-    "classify_su4",
-    "are_locally_equivalent",
-    "nonlocal_fingerprint",
-    "in_weyl_chamber",
-    "phase_aligned_operator_error",
-    # measures
-    "concurrence_pure",
-    "is_separable_pure",
-    "schmidt_values_pure",
-    "von_neumann_entropy",
-    "entanglement_entropy_pure",
-    # classification
-    "operator_schmidt_rank",
-    "is_local_product_operator",
-    "local_product_factors",
-    # analysis API
-    "EntanglementMetric",
-    "EntanglementAnalysisResult",
-    "EntanglementAnalysisOptions",
-    "analyze_entanglement",
-    "CouplingMetric",
-    "CouplingAnalysisResult",
-    "CouplingAnalysisOptions",
-    "PreservationAnalysisResult",
-    "analyze_circuit_coupling",
+    "ATOL", "RTOL", "I2", "I4", "X", "Y", "Z", "XX", "YY", "ZZ",
+    "CNOT", "CZ", "SWAP", "ISWAP",
+    "ket00", "ket01", "ket10", "ket11", "basis_state", "computational_basis",
+    "kron", "local_unitary", "apply_unitary",
+    "normalize_state", "state_from_amplitudes", "density_matrix", "reduced_density_matrix",
+    "xx_rotation", "yy_rotation", "zz_rotation", "canonical_entangler",
+    "cartan_core_from_weyl", "weyl_to_rotation_coordinates", "rotation_to_weyl_coordinates",
+    "CONVENTION_VERSION", "QuaternionCartanBlock", "SU4Classification",
+    "VerifiedSU4Decomposition", "decompose_su4", "decompose_su4_verified",
+    "reconstruct_su4", "classify_su4", "are_locally_equivalent",
+    "nonlocal_fingerprint", "in_weyl_chamber", "phase_aligned_operator_error",
+    "concurrence_pure", "is_separable_pure", "schmidt_values_pure",
+    "von_neumann_entropy", "entanglement_entropy_pure",
+    "operator_schmidt_rank", "is_local_product_operator", "local_product_factors",
+    "EntanglementMetric", "EntanglementAnalysisResult", "EntanglementAnalysisOptions",
+    "analyze_entanglement", "CouplingMetric", "CouplingAnalysisResult",
+    "CouplingAnalysisOptions", "PreservationAnalysisResult", "analyze_circuit_coupling",
     "analyze_optimization_preservation",
-    # validation
-    "assert_state_vector",
-    "assert_two_qubit_operator",
-    "assert_single_qubit_operator",
-    "is_unitary",
-    "is_su4",
-    "normalize_global_phase",
-    # adapter
-    "rqm_core_available",
-    "local_from_quaternions",
-    "su2_from_quaternion_components",
-    "su2_matrix_to_quaternion",
-    "quaternion_to_su2_matrix",
-    "normalize_local_su2_factor",
+    "BellHinge", "AxisHinge", "CartanRelation", "RelationalLevel", "RelationalOperator",
+    "IDENTITY_QUATERNION", "relational_level", "compose_relations",
+    "promote_with_local_frames", "compress_unitary",
+    "assert_state_vector", "assert_two_qubit_operator", "assert_single_qubit_operator",
+    "is_unitary", "is_su4", "normalize_global_phase",
+    "rqm_core_available", "local_from_quaternions", "su2_from_quaternion_components",
+    "su2_matrix_to_quaternion", "quaternion_to_su2_matrix", "normalize_local_su2_factor",
     "canonicalize_quaternion_sign_with_phase",
 ]
